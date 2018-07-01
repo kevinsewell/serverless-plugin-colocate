a2d0018ddc9d8ca94daadfe9ff62ef3829e48d55
Kevin Sewell <kevin.w.sewell@gmail.com>
2018-07-01 16:52:10 -0400

    Using Globby to include/exclude files

--- example/package2/goodbye.js
+++ example/package2/goodbye.js
@@ -1,8 +1,16 @@
+"use strict";
 
 let process = require("process");
 
 let message = process.env["GOODBYE_MESSAGE"];
 
+/**
+ * Handle Lambda Invoke Request
+ *
+ * @param event
+ * @param context
+ * @param callback
+ */
 exports.handle = (event, context, callback) => {
     callback(null, {
         statusCode: 200,
--- src/index.js
+++ src/index.js
@@ -1,38 +1,29 @@
 "use strict";
 
+/**
+ * @module serverless-plugin-colocate
+ *
+ * @see {@link https://serverless.com/framework/docs/providers/aws/guide/plugins/}
+ *
+ * @requires "bluebird"
+ * @requires "lodash"
+ * @requires "js-yaml"
+ * @requires "./lib/walkDirSync
+ * */
+
 const BbPromise = require("bluebird");
 const _ = require("lodash");
 const YAML = require("js-yaml");
-const walkDirSync = require("./lib/walkDirSync");
+const globby = require("globby");
 
-let shouldInclude = (filename, includes) => {
-
-    if (!includes || includes.length === 0) return true;
-
-    for (const include of includes) {
-        if (filename.index(include) >= 0) return true;
-    }
-
-    return false;
-};
-
-let shouldNotExclude = (filename, excludes) => {
-
-    if (!excludes || excludes.length === 0) return true;
-
-    for (const exclude of excludes) {
-        if (filename.indexOf(exclude) >= 0) return false;
-    }
-
-    return true;
-};
-
-let filenameEndsWithExtension = (filename, extension) =>
-    filename.indexOf("." + extension) === (filename.length - (extension.length + 1));
-
-let isYamlFile = (filename) =>
-    filenameEndsWithExtension(filename, "yml") || filenameEndsWithExtension(filename, "yaml");
-
+/**
+ * Update Handler file location with path from the configFragment
+ *
+ * @param functionName
+ * @param configFragment
+ *
+ * @param relativeConfigFragmentFilePath
+ */
 let correctHandlerLocation = (functionName, configFragment, relativeConfigFragmentFilePath) => {
 
     const function_ = configFragment.functions[functionName];
@@ -42,15 +33,47 @@ let correctHandlerLocation = (functionName, configFragment, relativeConfigFragme
     }
 };
 
+/**
+ * Add Exclude Pattern
+ *
+ * @param pattern
+ * @param patterns
+ */
+let addExcludePattern = (pattern, patterns) => {
+    if (pattern.charAt(0) !== "!") {
+        patterns.push(`!${pattern}`);
+    } else {
+        patterns.push(pattern.substring(1));
+    }
+};
+
+/**
+ * Add Include Pattern
+ *
+ * @param pattern
+ * @param patterns
+ */
+let addIncludePattern = (pattern, patterns) => patterns.push(pattern);
+
+/**
+ * @classdesc Colocate you configuration and code
+ * @class ServerlessPluginColocate
+ */
 class ServerlessPluginColocate {
+
+    /**
+     *
+     * @param serverless
+     * @param options
+     */
     constructor(serverless, options) {
         this.serverless = serverless;
+
         this.options = options;
-        this.mergeCodeFragmentsIntoService();
 
         this.commands = {
             colocate: {
-                usage: "Colocate Configuration and Code",
+                usage: "Colocate you configuration and code",
                 lifecycleEvents: ["colocate"],
                 commands: {
                     effective: {
@@ -62,17 +85,29 @@ class ServerlessPluginColocate {
         };
 
         this.hooks = {
-            "colocate:colocate": () => {
-                this.serverless.cli.generateCommandsHelp(["colocate"]);
-                return BbPromise.resolve();
-            },
-            "colocate:effective:effective": () => {
-                this.printEffectiveConfig();
-                return BbPromise.resolve();
-            }
+            "colocate:colocate": this.printColocateUsage.bind(this),
+            "colocate:effective:effective": this.printEffectiveConfig.bind(this)
         };
+
+        this.mergeCodeFragmentsIntoService();
     }
 
+    /**
+     * Print Colocate Usage
+     *
+     * @returns {Promise}
+     */
+    printColocateUsage() {
+        this.serverless.cli.generateCommandsHelp(["colocate"]);
+
+        return BbPromise.resolve();
+    }
+
+    /**
+     * Print Effective Serverless.yml
+     *
+     * @returns {Promise}
+     */
     printEffectiveConfig() {
 
         const effectiveServiceConfig = {};
@@ -86,15 +121,24 @@ class ServerlessPluginColocate {
         });
 
         this.serverless.cli.log("Effective serverless.yml:\n" + YAML.dump(effectiveServiceConfig));
+
+        return BbPromise.resolve();
     }
 
+    /**
+     * Merge all configuration fragments into service configuration
+     */
     mergeCodeFragmentsIntoService() {
         this.getConfigFragmentFilenames()
-            .forEach((codeFragmentFilename) =>
-                this.mergeCodeFragmentIntoService(codeFragmentFilename));
+            .then(filePaths => filePaths.forEach(this.mergeConfigFragmentIntoService.bind(this)));
     }
 
-    mergeCodeFragmentIntoService(configFilename) {
+    /**
+     * Merge configuration fragment into service configuration
+     *
+     * @param configFilename
+     */
+    mergeConfigFragmentIntoService(configFilename) {
 
         const servicePath = this.serverless.config.servicePath;
         const configFragmentFilePath = configFilename.substr(0, configFilename.lastIndexOf("/"));
@@ -106,6 +150,7 @@ class ServerlessPluginColocate {
             this.serverless.cli.log("Colocate is ignoring " + configFilename.replace(servicePath, ""));
             return;
         }
+
         delete configFragment.ignore;
 
         if (configFragment && configFragment.functions) {
@@ -117,18 +162,70 @@ class ServerlessPluginColocate {
         this.serverless.service = _.merge(this.serverless.service || {}, configFragment);
     }
 
+    /**
+     * Search service paths recursively for Serverless configration fragment files
+     *
+     * @returns {*}
+     */
     getConfigFragmentFilenames() {
 
         const servicePath = this.serverless.config.servicePath;
-        const files = walkDirSync(servicePath, {noLinks: true});
-        const includes = [];
-        const excludes = ["serverless.yml", "node_modules", ".serverless"];
 
-        return files
-            .filter(filename => isYamlFile(filename))
-            .filter(filename => shouldInclude(filename, includes))
-            .filter(filename => shouldNotExclude(filename, excludes));
+        const patterns = this.getCustomDefaultInclude() || ["**/*.yml", "**/*.yaml"];
+
+        const defaultExclude = this.getCustomDefaultExclude() ||
+            ["serverless.yml", "node_modules/**"];
+
+        defaultExclude.forEach((pattern) => addExcludePattern(pattern, patterns));
+
+        this.getCustomExclude()
+            .forEach((pattern) => addExcludePattern(pattern, patterns));
+
+        return globby(patterns, {
+            cwd: servicePath,
+            silent: true,
+            follow: true,
+            nodir: true
+        });
     };
+
+    /**
+     * Get Custom Parameters
+     *
+     * @returns {*}
+     */
+    getCustomParameters() {
+        return (this.serverless.service.custom && this.serverless.service.custom.colocate) || {};
+    }
+
+    /**
+     * Get Custom Default Includes
+     *
+     * @returns [*]
+     */
+    getCustomDefaultInclude() {
+        return (this.getCustomParameters().defaultInclude && this.getCustomParameters().defaultInclude.slice(0));
+    }
+
+    /**
+     * Get Custom Default Excludes
+     *
+     * @returns [*]
+     */
+    getCustomDefaultExclude() {
+        return (this.getCustomParameters().defaultExclude && this.getCustomParameters().defaultExclude.slice(0));
+    }
+
+    /**
+     * Get Custom Excludes
+     *
+     * @returns [*]
+     */
+    getCustomExclude() {
+        return (this.getCustomParameters().exclude && this.getCustomParameters().exclude.slice(0)) || [];
+    }
+
 }
 
+/** Export ServerlessPluginColocate class */
 module.exports = ServerlessPluginColocate;
